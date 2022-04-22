66ac44be0f8e55384eda8fba73bfe9e9cd331228
Benoit Turbang <benoit.turbang@vlaanderen.be>
2022-04-22 14:13:49 +0000

    update

--- models/2_stages/stg__voorkeuren.sql
+++ models/2_stages/stg__voorkeuren.sql
@@ -1,3 +1,3 @@
-select ld.mail, ld.lievelingsdier, lk.lievelingskleur
+select ld.mail, {{vertaling('ld.lievelingsdier')}} lievelingsdier_vertaald, lievelingsdier, lk.lievelingskleur
 from {{ ref('stg__lievelingsdieren') }} as ld
 left join {{ ref('stg__lievelingskleuren') }} as lk on lk.mail = ld.mail
